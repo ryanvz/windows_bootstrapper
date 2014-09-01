3f1081a3f6d2cf3db2033facacdb9088a8036595
Ryan Veazey <ryan.veazey@rackspace.com>
2014-08-31 20:02:58 -0500

    updated

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -15,3 +15,18 @@ $installed_deps = choco list -localonly | % { $_.split()[0] }
 $required_deps = @("git", "DotNet4.5", "microsoft-build-tools", "msdeploy", "NuGet.CommandLine", "ruby")
 
 $required_deps | ? { $installed_deps -notcontains $_ } | % { Instal-Dependancy $_ }
+
+$env:Path = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
+
+gem install bundle rake
+
+if(test-path ~\.ssh\id_rsa)
+{
+	    if($cert = Read-Host 'Paste your id_rsa key' -AsSecureString) { echo $cert >> ~/.ssh/id_rsa }
+}
+
+git clone git@github.com:Jungledisk/myjungledisk.com.git
+cd myjungledisk.com
+git submodule init
+git submodule update
+rake
