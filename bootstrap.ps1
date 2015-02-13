dde397346cced05cddd6b0307743f8569a1a51f9
Ryan Veazey <ryan.veazey@rackspace.com>
2015-02-13 14:18:29 -0600

    added more .net dev packages

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -5,12 +5,13 @@ if(-Not (Test-Path env:ChocolateyInstall))
     iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-choco install git DotNet4.5 microsoft-build-tools webdeploy NuGet.CommandLine ruby1.9 ruby.devkit notepadplusplus ConEmu GoogleChrome
+choco install git DotNet4.5 microsoft-build-tools windows-sdk-8.0 aspnetmvc4.install webdeploy NuGet.CommandLine ruby1.9 ruby.devkit notepadplusplus ConEmu GoogleChrome
 
 mkdir ~/.ssh
 echo $null >> ~/.ssh/id_rsa
 echo $null >> ~/.ssh/id_rsa.pub
 
 #refresh path to pick up ruby binaries
-$env:Path = [System.Environment]::GetEnvironmentVariable("PATH", "Machine")
+$env:Path=[System.Environment]::GetEnvironmentVariable("PATH","Machine") + ";C:\Program Filesx86)\IIS\Microsoft Web Deploy V3"
+[System.Environment]::SetEnvironmentVariable("PATH",$env:Path,"Machine")
 gem install bundle
