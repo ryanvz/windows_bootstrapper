cc84a2804f28319e81eac8e7d81184751def6c51
Ryan Veazey <ryan.veazey@gmail.com>
2015-11-17 15:31:32 -0600

    update paths; remove ssh stuff

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -7,11 +7,7 @@ if(-Not (Test-Path env:ChocolateyInstall))
 
 choco install git DotNet4.5 microsoft-build-tools windows-sdk-8.0 aspnetmvc4.install webdeploy NuGet.CommandLine ruby1.9 ruby.devkit notepadplusplus ConEmu GoogleChrome
 
-mkdir ~/.ssh
-echo $null >> ~/.ssh/id_rsa
-echo $null >> ~/.ssh/id_rsa.pub
-
 #refresh path to pick up ruby binaries
-$env:Path=[System.Environment]::GetEnvironmentVariable("PATH","Machine") + ";C:\Program Filesx86)\IIS\Microsoft Web Deploy V3"
+$env:Path=[System.Environment]::GetEnvironmentVariable("PATH","Machine") + ";C:\Program Files (x86)\IIS\Microsoft Web Deploy V3;C:\Program Files (x86)\MSBuild\12.0\Bin"
 [System.Environment]::SetEnvironmentVariable("PATH",$env:Path,"Machine")
 gem install bundle
